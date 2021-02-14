71acac44c625123b6498204438f83e048e3d7d49
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-02-14 21:04:07 +0100

    Create quotes.js
    List of quotes

---
+++ src/quotes.js
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    frase: "bla bla bla",
+    author: "author 1",
+  },
+  {
+    frase: "ble ble ble",
+    author: "author 2",
+  },
+  {
+    frase: "bli bli bli",
+    author: "author 3",
+  },
+  {
+    frase: "blo blo blo",
+    author: "author 4",
+  },
+  {
+    frase: "blu blu blu",
+    author: "author 5",
+  },
+];
